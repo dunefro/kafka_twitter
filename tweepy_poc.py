8b634d7df49bf90367c04fd41981fdb6823fa015
Vedant Pareek <dunefro@gmail.com>
2020-05-22 18:41:32 +0000

    Added details for Oauth and AppAuth

--- tweepy_poc.py
+++ tweepy_poc.py
@@ -10,6 +10,9 @@ consumer_secret = creds['consumer_secret']
 access_token = creds['access_token'] 
 access_token_secret = creds['access_token_secret']
 
+# OAuthHandler is used for user purpose that is why we need to also pass the access keys and tokens.
+# AppAuthHandler is used for public data
+
 # auth = OAuthHandler(consumer_key, consumer_secret)
 auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
 # auth.set_access_token(access_token, access_token_secret)
