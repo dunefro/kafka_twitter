ef097319fdc9748fd29326a31ea23bc15cbda2c4
Vedant Pareek <dunefro@gmail.com>
2020-05-22 17:54:40 +0000

    Modified toke_secret keyword

--- python_twitter.py
+++ python_twitter.py
@@ -8,12 +8,12 @@ print(creds)
 consumer_key = creds['consumer_key']
 consumer_secret = creds['consumer_secret']
 access_token = creds['access_token'] 
-access_secret_token = creds['access_secret_token']
+access_token_secret = creds['access_secret_token']
 
 api = twitter.Api(consumer_key=consumer_key,
                   consumer_secret=consumer_secret,
                   access_token_key=access_token,
-                  access_token_secret=access_secret_token)
+                  access_token_secret=access_token_secret)
 
 # # print(list(api.GetTrendsCurrent()))
 trends = api.GetTrendsWoeid(woeid='2282863')
