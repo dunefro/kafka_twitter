01dce3632fc7e69b7b580c5d321e399b1239b79f
Vedant Pareek <36420365+dunefro@users.noreply.github.com>
2020-06-02 23:07:21 +0530

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -2,6 +2,8 @@ from kafka import KafkaConsumer
 
 def _consume_tweet(topic):
     consumer = KafkaConsumer(topic,group_id=topic+'-'+'group',bootstrap_servers=['localhost:9092'],auto_offset_reset='earliest')
+    # Adding poll
+    # Adding subscribe
     for message in consumer:
         print ('%s:%d:%d: key=%s value=%s' % (message.topic, message.partition, message.offset, message.key, message.value))
     
