f1d5079bd2d3865af413babf68a024669907057c
Vedant Pareek <dunefro@gmail.com>
2020-06-08 17:20:32 +0000

    Consumer working properly

--- consumer.py
+++ consumer.py
@@ -7,10 +7,9 @@ def _group_name(topic):
 def _consume_tweet(topic):
     consumer = KafkaConsumer(topic,group_id=_group_name(topic),bootstrap_servers=['localhost:9092'],auto_offset_reset='earliest',
                             key_deserializer = lambda a: a.decode(), value_deserializer = lambda a: a.decode())
-    # Adding poll
-    # Adding subscribe
+    consumer.subscribe([topic])
     # consumer.seek_to_beginning()
     for message in consumer:
         print ('%s:%d:%d: key=%s value=%s' % (message.topic, message.partition, message.offset, message.key, message.value))
     
-_consume_tweet('modi')
+_consume_tweet('trump')
